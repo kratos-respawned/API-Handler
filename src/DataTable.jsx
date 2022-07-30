21857e67ecd935dc99c93b513205b65e71df618d
Gaurav Bhandari <sbhandari1907@protonmail.com>
2022-07-30 17:02:24 +0530

    minor bug fix

--- src/DataTable.jsx
+++ src/DataTable.jsx
@@ -1,10 +1,9 @@
-import axios from "./axios.js";
 import { useEffect, useRef, useState } from "react";
 import Table from "react-bootstrap/Table";
 import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/esm/Button.js";
 import Form from "react-bootstrap/Form";
-import Instance from "./Axios.js";
+import Instance from "./Axios";
 const DataTable = () => {
   let [data, setData] = useState([]);
   let [name, setName] = useState();
